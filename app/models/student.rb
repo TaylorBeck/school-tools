1f14a4345c3a448077f9f90187a0e70ef8c7a012
Taylor Beck <beck.taylorg@gmail.com>
2022-06-06 00:27:04 -0700

    Added student create form

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
+  validates :first_name, :last_name, :email, presence: true
+
   def name
     "#{self.first_name} #{self.last_name}"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'students#index'
 
-  resources :students, only: [:index]
+  resources :students, only: [:index, :new, :create]
 end
